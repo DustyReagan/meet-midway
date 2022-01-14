4cd7077020f2e03fa290ce7272ee22c7d4c79b9c
Dusty Reagan <dustyreagan@MacBook-Pro.localdomain>
2022-01-14 15:43:43 -0600

    Added map component.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "./app.css";
+import { Icon } from "leaflet";
+
 function App() {
   return (
     <div>
-      <h2>Hello world!</h2>
+      <h1>Hello map!</h1>
+      <MapContainer
+        center={[30.304129, -97.7363697]}
+        zoom={15}
+        scrollWheelZoom={true}
+      >
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        />
+      </MapContainer>
     </div>
   );
 }
