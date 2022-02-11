b92eb3e36d914c1e34403ff2a3ff09f4d8befa8e
Dusty Reagan <dusty@dustyreagan.com>
2022-02-11 17:05:31 -0600

    Updated formating.

--- src/components/Location/LocationForm.js
+++ src/components/Location/LocationForm.js
@@ -1,37 +1,42 @@
-import React, { useState } from "react";
+import React, {useState} from 'react';
 
-import "./LocationForm.css";
+import './LocationForm.css';
 
 const LocationForm = (props) => {
-  const [enteredCoordinate, setEnteredCoordinate] = useState('');
+	const [enteredCoordinate, setEnteredCoordinate] = useState('');
 
-  const coordinateChangeHandler = (event) => {
-    setEnteredCoordinate(event.target.value);
-  };
+	const coordinateChangeHandler = (event) => {
+		setEnteredCoordinate(event.target.value);
+	};
 
-  const submitHandler = (event) => {
-    event.preventDefault();
+	const submitHandler = (event) => {
+		event.preventDefault();
 
-    // Turn a string 1,1 into an array [1,1].
-    const coordinateArray = JSON.parse('[' + enteredCoordinate + ']');
-    const locationData = {
-      lat: coordinateArray[0],
-      lng: coordinateArray[1],
-    };
+		// Turn a string 1,1 into an array [1,1].
+		const coordinateArray = JSON.parse('[' + enteredCoordinate + ']');
+		const locationData = {
+			lat: coordinateArray[0],
+			lng: coordinateArray[1],
+		};
 
-    props.onSaveLocationData(locationData);
-    setEnteredCoordinate('');
-  };
+		props.onSaveLocationData(locationData);
+		setEnteredCoordinate('');
+	};
 
-  return (
-    <form onSubmit={submitHandler}>
-      <div className="location">
-        <label htmlFor="coordinate">Coordinate</label>
-        <input type="text" value={enteredCoordinate} onChange={coordinateChangeHandler} id="coordinate"></input>
-        <button type="submit">Add</button>
-      </div>
-    </form>
-  );
+	return (
+		<form onSubmit={submitHandler}>
+			<div className="location">
+				<label htmlFor="coordinate">Coordinate</label>
+				<input
+					type="text"
+					value={enteredCoordinate}
+					onChange={coordinateChangeHandler}
+					id="coordinate"
+				></input>
+				<button type="submit">Add</button>
+			</div>
+		</form>
+	);
 };
 
 export default LocationForm;
--- src/components/Location/NewLocation.js
+++ src/components/Location/NewLocation.js
@@ -1,19 +1,19 @@
-import LocationForm from "./LocationForm";
+import LocationForm from './LocationForm';
 
 const NewLocation = (props) => {
-  const saveLocationDataHandler = (enteredCoordinate) => {
-    const coordinateData = {
-      ...enteredCoordinate,
-      id: Math.random().toString(),
-    };
-    props.onAddLocation(coordinateData);
-  };
+	const saveLocationDataHandler = (enteredCoordinate) => {
+		const coordinateData = {
+			...enteredCoordinate,
+			id: Math.random().toString(),
+		};
+		props.onAddLocation(coordinateData);
+	};
 
-  return (
-    <div>
-      <LocationForm onSaveLocationData={saveLocationDataHandler} />
-    </div>
-  );
+	return (
+		<div>
+			<LocationForm onSaveLocationData={saveLocationDataHandler} />
+		</div>
+	);
 };
 
 export default NewLocation;
